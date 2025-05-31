3e37a1e987cfa7f55f68fb0a71dc1f74bcc39e9b
Stramsky <suniyab43@gmail.com>
2025-06-01 03:53:40 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 
-import './styles.css';
 import './globals.css';
-import { ReactNode, useEffect } from 'react';
+import { ReactNode } from 'react';
 
 export const metadata = {
   title: 'ZHFLIM Player',
@@ -9,12 +8,6 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
-  useEffect(() => {
-    if ('serviceWorker' in navigator && process.env.NODE_ENV === 'production') {
-      navigator.serviceWorker.register('/sw.js');
-    }
-  }, []);
-
   return (
     <html lang="zh-CN">
       <body>{children}</body>
